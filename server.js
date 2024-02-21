0a30d82c1a9cd1e36a035cf7dc58c41a96c18e4b
Rishabh <rishabhgoel2020@gmail.com>
2024-02-21 12:43:45 +0530

    Add RAW code for CORS policy configuration

--- server.js
+++ server.js
@@ -12,7 +12,20 @@ import apiDocs from "./swagger.json" assert {type: "json"};
 const PORT = 3000;
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+
+//CORS policy Configuration
+
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:5500") //we can add * to give access to all clients
+     // doing this to res because this access will be given by the server not by the client
+     // return of for preflight request
+//     res.header("Access-Control-Allow-Headers", "*");
+//     res.header("Access-Control-Allow-Methods", "*") // that can be used to add multiple methods that youwant a specific client to perform like get patch post etc.
+//     if(req.method == "OPTIONS") res.sendStatus(200)
+//     next();
+// })
+app.use(cors());
+
 // ek particular pattern ki request ko handle krne k liye.. hum routes bna dete h or ek middleware ki tarah use use kr lete h
 
 // MiddleWares implementation
